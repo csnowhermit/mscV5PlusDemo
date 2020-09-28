8a9ed92314f7b483fce79842c4230555db1f3fbb
920164255@qq.com <920164255@qq.com>
2020-09-28 10:59:26 +0800

    update: 更新SDK

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # 讯飞离线语音改造
 
-程序入口：com.iflytek.driver.IatActivitySocketInMain
+自动循环录循环识别程序入口：com.iflytek.driver.IatActivitySocketInMain
+收到信号后启动录程序入库：com.iflytek.nio.IatNioSpeech
 
-在Android studio中打开，在本地的目录：D:/workspace/xunfei_offline/Android_esriat_exp1/sample/mscV5PlusDemo/
+在Android studio中打开，在本地的目录：D:/workspace/xunfei_offline/mscV5PlusDemo/
--- src/main/java/com/iflytek/nio/IatNioSpeech.java
+++ src/main/java/com/iflytek/nio/IatNioSpeech.java
@@ -1,9 +1,15 @@
 package com.iflytek.nio;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.os.StrictMode;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
+import android.view.View;
 
 import com.alibaba.fastjson.JSON;
 import com.iflytek.cloud.ErrorCode;
@@ -22,17 +28,32 @@ import com.iflytek.speech.util.JsonParser;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
-public class IatNioSpeech extends Activity implements Runnable {
+/**
+ * 主线程中跑mIat
+ * 开启子线程接受“startIAT”消息：TcpNioServer，收到startIAT消息后转为消息代码1，发送到主线程
+ */
+public class IatNioSpeech extends Activity implements View.OnClickListener {
     private static final long serialVersionUID = 1L;
 
     private static String TAG = "nio.TcpNioServer2Semantics";
     private static String daotai_id = "center01";    //导台ID，标识不同朝向的
-    private String host = "127.0.0.1";
+    private String host = "192.168.0.27";
     private int port = 50007;
     private Socket socket;    //与语义端通信
     private OutputStream outputStream;    // 网络输出流
@@ -41,8 +62,12 @@ public class IatNioSpeech extends Activity implements Runnable {
     private Map<String, String> mParamMap = new HashMap<String, String>();
     private static final String VAL_TRUE = "1";
 
+    Handler mainHandler = null;
 
-    public IatNioSpeech() {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
                 .detectDiskReads().detectDiskWrites().detectNetwork()
                 .penaltyLog().build());
@@ -72,6 +97,23 @@ public class IatNioSpeech extends Activity implements Runnable {
             e.printStackTrace();
         }
         System.out.println("已连接至语义端");
+        Thread thread = new Thread(new TcpNioServer());
+
+        thread.start();    // 打开子线程
+        mainHandler = new Handler(){    // 接受子线程发来的消息，并进行处理
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                int sign = msg.arg1;
+                if (sign == 1) {
+                    System.out.println(String.format("收到信号 %d，等待开启监听", sign));
+                    action();
+                } else {
+                    System.out.println(String.format("信号错误：%d", sign));
+                }
+            }
+        };
+
     }
 
     private InitListener mInitListener = new InitListener() {
@@ -297,10 +339,146 @@ public class IatNioSpeech extends Activity implements Runnable {
         return tempBuffer.toString();
     }
 
-
     @Override
-    public void run() {
-        action();
+    public void onClick(View v) {
+
+    }
+
+    // 嵌套子线程，用于
+    class TcpNioServer extends Thread{
+        // 在本地接受后端“开始听写”信号的
+        private int listenedPort = 50008;
+        private InetSocketAddress localAddress;    // 服务器地址
+
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        @Override
+        public void run() {
+            super.run();
+            this.localAddress = new InetSocketAddress(listenedPort);
+
+            Charset charset = Charset.forName("UTF-8");
+
+            ServerSocketChannel serverSocketChannel = null;
+            Selector selector = null;
+            Random random = new Random();
+
+            try {
+                serverSocketChannel = ServerSocketChannel.open();    //打开服务端通道
+                serverSocketChannel.configureBlocking(false);    //设置为非阻塞
+                selector = Selector.open();    //打开选择器
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    serverSocketChannel.bind(localAddress, 100);    //绑定服务器地址，最多可连接100个客户端
+                }
+                serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);    //将选择器注册到通道，对accept事件感兴趣
+
+                System.out.println("TcpNioServer started successful");
+            } catch (IOException e) {
+                System.out.println("TcpNioServer started failed: " + e.getMessage());
+            }
+
+            try {
+                while (!Thread.currentThread().isInterrupted()) {
+                    int n = selector.select();    //从选择器中取，如果没有这步，则没有结果
+                    if (n == 0) {    //0代表没有任何事件进来
+                        continue;
+                    }
+
+                    Set<SelectionKey> selectionKeySet = selector.selectedKeys();
+                    Iterator<SelectionKey> iterator = (Iterator<SelectionKey>) selectionKeySet.iterator();
+                    SelectionKey selectionKey = null;
+
+                    while (iterator.hasNext()) {
+                        selectionKey = iterator.next();
+                        iterator.remove();    //移除，以免重复取出
+
+                        try {
+                            //处理客户端的连接
+                            if (selectionKey.isAcceptable()) {
+                                SocketChannel socketChannel = serverSocketChannel.accept();   //等到客户端的channel
+                                socketChannel.configureBlocking(false);    //设置为非阻塞
+
+                                int interestOps = SelectionKey.OP_READ;    //对读事件感兴趣
+                                socketChannel.register(selector, interestOps, new Buffers(256, 256));
+                                System.out.println("Server端：accept from: " + socketChannel.getRemoteAddress());
+                            }
+
+                            //处理可读消息
+                            if (selectionKey.isReadable()) {
+                                //先准备好缓冲区
+                                Buffers buffers = (Buffers) selectionKey.attachment();
+                                ByteBuffer readBuffer = buffers.getReadBuffer();
+                                ByteBuffer writeBuffer = buffers.getWriteBuffer();
+
+                                SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+
+                                socketChannel.read(readBuffer);    //将数据从底层缓冲区读到自定义缓冲区中
+                                readBuffer.flip();
+
+                                CharBuffer charBuffer = charset.decode(readBuffer);    //解码接收到的数据
+                                String sign = new String(charBuffer.array()).trim();
+                                System.out.println("Server端：接收到客户端的消息：" + sign);
+                                if ("startIAT".equals(sign)){
+                                    Message msg = Message.obtain();
+                                    msg.arg1 = 1;    // startIAT对应的信号为1
+                                    mainHandler.sendMessage(msg);
+                                }
+
+                                readBuffer.rewind();    //重置缓冲区指针位置
+
+                                //准备给客户端返回数据
+                                writeBuffer.put("Echo from Server: ".getBytes("UTF-8"));
+                                writeBuffer.put(readBuffer);    //将客户端发来的数据原样返回
+                                writeBuffer.put("\n".getBytes("UTF-8"));
+
+                                readBuffer.clear();    //清空读缓冲区
+                                selectionKey.interestOps(selectionKey.interestOps() | SelectionKey.OP_WRITE);    //为通道添加写事件
+                            }
+
+                            //处理可写消息
+                            if (selectionKey.isWritable()) {
+                                Buffers buffers = (Buffers) selectionKey.attachment();
+                                ByteBuffer writeBuffer = buffers.getWriteBuffer();
+                                writeBuffer.flip();
+
+                                SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+
+                                int len = 0;
+                                while (writeBuffer.hasRemaining()) {
+                                    len = socketChannel.write(writeBuffer);
+                                    if (len == 0) {    //说明底层socket写缓冲区已写满
+                                        break;
+                                    }
+                                }
+
+                                writeBuffer.compact();
+
+                                if (len != 0) {    //说明数据已全部写入到底层的socket写缓冲区
+                                    selectionKey.interestOps(selectionKey.interestOps() & (~SelectionKey.OP_WRITE));    //取消通道的写事件
+                                }
+                            }
+                        } catch (IOException e) {
+                            System.out.println("Server encounter client error: " + e.getMessage());
+                            selectionKey.cancel();
+                            selectionKey.channel().close();
+                        }
+                    }
+                    Thread.sleep(random.nextInt(500));
+                }
+            } catch (InterruptedException e) {
+                System.out.println("serverThread is interrupted: " + e.getMessage());
+            } catch (IOException e) {
+                System.out.println("serverThread selecotr error: " + e.getMessage());
+            } finally {
+                try {
+                    selector.close();
+                } catch (IOException e) {
+                    System.out.println("selector close failed");
+                } finally {
+                    System.out.println("server closed successful");
+                }
+            }
+        }
     }
 }
 
